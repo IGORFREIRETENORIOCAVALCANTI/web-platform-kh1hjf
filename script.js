ba7787ea80dbad0beb52ded05f70bc35e18ffc06
dokebu <gamerdokebu@gmail.com>
2022-04-05 08:49:20 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello!')
+var cliques = 0;
+function clica() {
+    cliques += 1;
+    document.getElementById("cliques").innerHTML = cliques;
+};
+function zera(){
+  cliques =0;
+  document.getElementById("cliques").innerHTML = cliques;
+}
